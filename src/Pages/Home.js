8931838af7af80877a574ac80eb0e5c774d09ca6
BalaDokonaly <balaji.r@allmasters.ai>
2024-07-24 19:18:27 +0530

    fixed bugs

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -57,7 +57,7 @@ function Home() {
   }, []);
 
   return (
-    <main className="container mx-auto max-width md:flex justify-between items-center">
+    <main className="container mx-auto mainsection max-width md:flex justify-between items-center">
       <div>
         <h1
           ref={h11}
