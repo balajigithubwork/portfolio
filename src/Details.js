efc7946274412d346854065469799faa0e2a97c9
BalaDokonaly <balaji.r@allmasters.ai>
2024-07-24 16:50:39 +0530

    portfolio

---
+++ src/Details.js
@@ -0,0 +1,95 @@
+// Enter all your detials in this file
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Balaji R",
+  tagline: "Frontend Dev | UI/UX",
+  img: profile,
+  about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/balaji111299/",
+  github: "https://github.com/balajigithubwork",
+  email: "rbalaji111299@gmail.com",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Developer",
+    Company: `Dokonaly Cargo Management Service Private Limited`,
+    Location: "Chennai",
+    Type: "Full Time",
+    Duration: "Sep 2022 - Present",
+  },
+  {
+    Position: "UI Developer",
+    Company: `M2P Fintech`,
+    Location: "Chennai",
+    Type: "Internship",
+    Duration: "Mar 2022 - Sep 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Electronics and Communication Engineering",
+    Company: `Maha Barathi Engineering College`,
+    Location: "Chinnasalem",
+    Type: "Full Time",
+    Duration: "Aug 2017 - Mar 2021",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
