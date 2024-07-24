0f723cdfe7ae090dd7752647478006c09bcc7c59
BalaDokonaly <balaji.r@allmasters.ai>
2024-07-24 18:25:25 +0530

    fixed bugs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,6 +12,30 @@ function Header() {
   return (
     <header className="container mx-auto md:flex justify-between py-2 max-width">
       <div className="flex justify-between items-center py-2 md:py-10">
+        <NavLink to="/" className='text-lg font-bold'>Portfolio</NavLink>
+        <div onClick={toggleClass} className="cursor-pointer">
+          <svg
+            className="stroke-dark-heading dark:stroke-white md:hidden"
+            width="25"
+            height="20"
+            viewBox="0 0 16 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.4375 1.3125H14.5625M1.4375 11.3125H14.5625H1.4375ZM1.4375 6.3125H14.5625H1.4375Z"
+              strokeWidth="1.875"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
+      <nav
+        className={` ${
+          !isOpen ? "hidden" : null
+        } text-center md:flex justify-between`}
+      >
         <ul className="dark:text-light-content font-medium md:flex items-center md:space-x-5 md:mr-10">
           <li className="pb-1 md:pb-0">
             <NavLink to="/" onClick={toggleClass}>
@@ -33,13 +57,12 @@ function Header() {
               Projects
             </NavLink>
           </li>
+          <li>
+            <NavLink to="/contact" onClick={toggleClass}>
+              Contact
+            </NavLink>
+          </li>
         </ul>
-      </div>
-      <nav
-        className={` ${
-          !isOpen ? "hidden" : null
-        } text-center md:flex justify-between`}
-      >
         <ul className="flex justify-evenly items-center my-5 md:my-0 md:space-x-5 md:mr-5">
           <li>
             <a href={linkdein} target="_blank" rel="noreferrer noopener">
